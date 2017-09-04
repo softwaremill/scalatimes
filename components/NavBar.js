1e8a583e5fa28bd2b9157ab24cc69dcb6100f41b
Michal Chmielarz <michal.chmielarz@gmail.com>
2017-09-04 12:58:38 +0200

    NavBar layout

--- components/NavBar.js
+++ components/NavBar.js
@@ -2,41 +2,43 @@ import Link from "next/link";
 
 export default () => {
   return (
-    <nav className="navbar navbar-expand-lg">
-      <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav">
-          <li>
-            <Link href="/">
-              <a className="nav-link">Latest news</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/all" prefetch>
-              <a className="nav-link">All news</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/promote" prefetch>
-              <a className="nav-link">Promote your event</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/banners" prefetch>
-              <a className="nav-link">Get banner</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact" prefetch>
-              <a className="nav-link">Contact</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="http://www.softwaremill.com" prefetch>
-              <a className="nav-link">HIRING</a>
-            </Link>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    <div className="row no-gutters justify-content-center mt-3">
+      <nav className="navbar navbar-expand-lg mt-5">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li>
+              <Link href="/">
+                <a className="nav-link mx-4">Latest news</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/all" prefetch>
+                <a className="nav-link mx-4">All news</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/promote" prefetch>
+                <a className="nav-link mx-4">Promote your event</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/banners" prefetch>
+                <a className="nav-link mx-4">Get banner</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" prefetch>
+                <a className="nav-link mx-4">Contact</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="http://www.softwaremill.com" prefetch>
+                <a className="nav-link mx-4">HIRING</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
   );
 };
