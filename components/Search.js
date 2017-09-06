9c8d311e385009a32f2dd02adda8c9127b0c86f4
Michal Chmielarz <michal.chmielarz@gmail.com>
2017-09-06 16:56:04 +0200

    Search wrapped with container div

--- components/Search.js
+++ components/Search.js
@@ -11,13 +11,15 @@ export default class Search extends Component {
 
     render() {
         return (
-            <div className="search">
-                <div className="col-md-10 col-sm-12 mx-auto">
-                    <div className="has-feedback sm-w-100 xs-w-75">
-                        <input type="text" className="py-xs-1" placeholder="Search"
-                               ref={(input) => {this.phrase = input;}}
-                               onKeyPress={this.handleSearch}/>
-                        <span className="glyphicon glyphicon-search form-control-feedback search-icon"></span>
+            <div className="container">
+                <div className="search">
+                    <div className="col-md-10 col-sm-12 mx-auto">
+                        <div className="has-feedback sm-w-100 xs-w-75">
+                            <input type="text" className="py-xs-1" placeholder="Search"
+                                   ref={(input) => {this.phrase = input;}}
+                                   onKeyPress={this.handleSearch}/>
+                            <span className="glyphicon glyphicon-search form-control-feedback search-icon"></span>
+                        </div>
                     </div>
                 </div>
             </div>
