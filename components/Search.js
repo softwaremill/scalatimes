45d6247a685bc386aade634f12b69f1305bf467c
Michal Chmielarz <michal.chmielarz@gmail.com>
2017-09-11 16:47:31 +0200

    Formatting

--- components/Search.js
+++ components/Search.js
@@ -12,12 +12,12 @@ export default class Search extends Component {
     render() {
         return (
             <div className="search">
-                    <div className="col-xs-12 col-sm-10 has-feedback xs-w-75 mx-auto search-max-width">
-                        <input type="text" className="py-1 px-3" placeholder="Search"
-                               ref={(input) => {this.phrase = input;}}
-                               onKeyPress={this.handleSearch}/>
-                        <span className="glyphicon glyphicon-search form-control-feedback search-icon"></span>
-                    </div>
+                <div className="col-xs-12 col-sm-10 has-feedback xs-w-75 mx-auto search-max-width">
+                    <input type="text" className="py-1 px-3" placeholder="Search"
+                           ref={(input) => {this.phrase = input;}}
+                           onKeyPress={this.handleSearch}/>
+                    <span className="glyphicon glyphicon-search form-control-feedback search-icon"></span>
+                </div>
             </div>
         )
     }
