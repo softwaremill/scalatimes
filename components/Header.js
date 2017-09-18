6d9b46f993b6b9f41b5bcd2009c819969c893bee
Michal Chmielarz <michal.chmielarz@gmail.com>
2017-09-18 11:58:42 +0200

    children argument removed

--- components/Header.js
+++ components/Header.js
@@ -3,7 +3,7 @@ import NavBar from "./NavBar";
 import Title from "./Title";
 import Subscribe from "./Subscribe";
 
-export default ({ children }) => {
+export default () => {
     return (
     <div>
       <Head>
