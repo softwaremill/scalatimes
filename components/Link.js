3686ce0fb3845240bbd51f868810d8000423756a
Michal Chmielarz <michal.chmielarz@gmail.com>
2017-10-06 16:45:37 +0200

    Preventing empty TT handles

--- components/Link.js
+++ components/Link.js
@@ -3,7 +3,7 @@ export default (props) => {
         <div className="link">
             <div><a href={props.url}><span className="link-title">{props.title}</span></a></div>
             <div className="link-desc">{props.description}</div>
-            <div className="link-author">~{props.author} (@{props.handle})</div>
+            <div className="link-author">~{props.author}{props.handle !== '' && "(@"+props.handle+")"}</div>
         </div>
     )
 }
