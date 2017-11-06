4d78ef2e6cb47e52853778881fc03b42d5856765
Michal Ostruszka <michal.ostruszka@gmail.com>
2017-11-06 15:46:17 +0100

    Components cleanup

--- layouts/main.js
+++ layouts/main.js
@@ -1,7 +1,5 @@
 import React from "react"
 import Head from "next/head"
-import Header from "../components/Header"
-import Footer from "../components/Footer"
 import Link from "next/link";
 
 const handleSubmit = () => {};
